0a57dccb0afd8030c7a9c0bdf85f342611890d19
Mariel_Druetta <marieldruetta@gmail.com>
2023-01-29 03:39:54 -0300

    Homework 03 Terminada

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,53 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+  var suma= 0;
+  for(var i= 1; i < 11; i++){
+    suma= suma + i;
+  }
+  return suma;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var nuevoarray= [];
+  for(var i= 0; i< array.length; i++){
+    if(array[i] % 2 === 0){
+      nuevoarray.push(array[i]);
+    }
+  }
+  return nuevoarray;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var nuevoarray= [];
+  for(var i= 0; i< array.length; i++){
+    nuevoarray.push(Math.pow(array[i],2))
+  }
+  return nuevoarray;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var sumar= 0;
+  for(var i= 0; i< array.length; i++){
+    sumar= sumar + array[i];
+  }
+  return sumar;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var str= num.toString();
+  return str.length;
 }
   
   
