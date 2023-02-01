af2e8a28e5e82c6918e3e7d34a8bea0d2a28da61
Mariel_Druetta <marieldruetta@gmail.com>
2023-02-01 01:36:24 -0300

    Homework 06 Terminada

--- 06-JS-VI/homework/homework.js
+++ 06-JS-VI/homework/homework.js
@@ -59,7 +59,7 @@ function filter(array) {
   nuevoArray= [];
   for(var i= 0; i< array.length; i++){
     if(array[i][0] === "a"){
-      nuevoArray.push(array[i])
+      nuevoArray.push(array[i]);
     }
   }
   return nuevoArray; 
